4bc42e3b1bf4067416c0ff8470790c5734d78e6a
Sven Van Gastel <sven.v.gastel@gmail.com>
2022-05-04 17:12:40 +0200

    code van @GetMapping inkorten met @RequestMapping

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -4,6 +4,7 @@ import be.vdab.luigi.domain.Pizza;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
@@ -11,6 +12,8 @@ import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
+// Met @RequestMapping voer je de domeinnaam in waar @GetMapping op verder gaat.
+@RequestMapping("pizzas")
 public class PizzaController {
 // aanmaken van een array van pizzas
     private final Pizza[] allePizzas = {
@@ -18,7 +21,7 @@ public class PizzaController {
             new Pizza(2 ,"Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)};
     // De method op de volgende regel (findAll) verwerkt GET request naar de URL /pizzas
-    @GetMapping("/pizzas")
+    @GetMapping
     public ModelAndView findAll(){
         // viewName: de naam van de Thymeleaf pagina (pizzas.html)
         // modelName: de naam waaronder je data doorgeeft aan de Thymeleaf pagina.
@@ -33,7 +36,7 @@ public class PizzaController {
         return Arrays.stream(allePizzas).filter(pizza->pizza.getId()==id).findFirst();
     }
     //Onderstaande method verwerkt GET request naar URL's die passen bij de URL template pizzas/{id}
-    @GetMapping("/pizzas/{id}")
+    @GetMapping("{id}")
     // @PathVariable = Spring vult de parameter id met de waarde van de path variabele met dezelfde
     // naam(id). Als de URl pizzas/1 is, vult Spring id met 1.
     public ModelAndView findById(@PathVariable long id){
@@ -42,7 +45,12 @@ public class PizzaController {
         // Je roept de findByIdHelper method op en je gebruikt id als parameter. Als je de pizza vindt,
         // geef je die door aan de Thymelead pagina onder de naam pizza.
         findByIdHelper(id).ifPresent(gevondenPizza ->
-                modelAndView.addObject("pizza", gevondenPizza));
+                /*
+                Spring kijkt naar het type van gevondenPizza. Dit is de class Pizza.
+                Spring wijzigt de 1e letter daarvan naar kleine letter: pizza.
+                Spring geeft de data onde die naam (pizza) aan de Thymeleaf pagina.
+                 */
+                modelAndView.addObject(gevondenPizza));
         return modelAndView;
     }
 }
